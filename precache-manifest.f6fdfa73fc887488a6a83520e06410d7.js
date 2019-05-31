50b9f2a2d02782e1c82297af218a2438d8bdd091
Mohd <fmohd195@gmail.com>
2019-06-01 02:41:42 +0530

    Updates

---
+++ precache-manifest.f6fdfa73fc887488a6a83520e06410d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc59f7fd31b6dd868571ce0d9c02f3c",
+    "url": "/invockAssignment/index.html"
+  },
+  {
+    "revision": "a8086655750b39d76811",
+    "url": "/invockAssignment/static/js/2.2a738516.chunk.js"
+  },
+  {
+    "revision": "e2cd223441074d256549",
+    "url": "/invockAssignment/static/js/main.859efce9.chunk.js"
+  },
+  {
+    "revision": "cb9837f928dd0bd1f211",
+    "url": "/invockAssignment/static/js/runtime~main.392fed4e.js"
+  }
+]);
