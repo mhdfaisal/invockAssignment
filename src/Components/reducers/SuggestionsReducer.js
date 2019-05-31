3ebd1ea76e1a227710f2d29f4fb1b4c48ad9ced1
Mohd <fmohd195@gmail.com>
2019-06-01 03:00:05 +0530

    fetchSugestion bug fixed

--- src/Components/reducers/SuggestionsReducer.js
+++ src/Components/reducers/SuggestionsReducer.js
@@ -1,6 +1,6 @@
 const SuggestionsReducers = (state=[], action)=>{
     switch(action.type){
-        case "FETCH_SUGGESTIONS": return [...state,...action.payload]
+        case "FETCH_SUGGESTIONS": return [...action.payload]
         default : return state;
     }
 }
